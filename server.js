438f55c6be8daf741c6a5b7e7c15ce26b70f297d
Evlos <4tyle8@gmail.com>
2014-07-01 14:21:06 +0800

    heroku-lize & installed foreman

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
+var port = Number(process.env.PORT || 8080);
 var logger = require('morgan');
 var express = require('express');
 var app = express();
 
 app.use(logger());
 app.use(express.static(__dirname + '/assets'));
-app.use(function(req, res){
+
+app.get('/', function(req, res) {
   res.send('Running ...');
 });
 
-var server = app.listen(8080, function() {
+var server = app.listen(port, function() {
     console.log('Listening on port %d', server.address().port);
 });
