446d37d9236b73f2be41f6301cf83b7fc6b5aa9d
Evlos <4tyle8@gmail.com>
2014-07-01 16:11:51 +0800

    completed output progress

--- server.js
+++ server.js
@@ -1,13 +1,31 @@
 var port = Number(process.env.PORT || 8080);
+
+var fs = require('fs');
+var parseurl = require('parseurl');
 var logger = require('morgan');
 var express = require('express');
 var app = express();
 
 app.use(logger());
-app.use(express.static(__dirname + '/assets'));
 
 app.get('/', function(req, res) {
   res.send('Running ...');
+});
+
+app.get('/*', function(req, res) {
+  var path = parseurl(req).pathname;
+  var data;
+  try {
+    data = fs.readFileSync('assets/' + path.replace('/', '') + '.svg');
+    res.writeHead(200, {'Content-Type': 'image/svg+xml'});
+    res.end(data);
+  } catch (e) {
+    if (e.code === 'ENOENT') {
+      res.send('File not found!');
+    } else {
+      throw e;
+    }
+  }
 });
 
 var server = app.listen(port, function() {
